927dacf54825073bec549e74bf0c01273c221f0f
Evgeny Kuznetsov <deadmaster.gm@yandex.ru>
2026-09-26 12:47:39 +0500

    Create method ParseDate and switchCase

--- Schedule.cs
+++ Schedule.cs
@@ -18,26 +18,27 @@ namespace Test
 
         public Schedule(string scheduleString)
         {
-            string[] parts = scheduleString.Split(' ');
-            string[] dateParts = parts[0].Split('.');
-            string[] timeParts = parts[2].Split(':');
-            string[] secondParts = timeParts[2].Split('.');
+            string[] parts = scheduleString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-            _years = ParsePart(dateParts[0], 2000, 2100);
-            _months = ParsePart(dateParts[1], 1, 12);
-            _days = ParsePart(dateParts[2], 1, 32);
-            _dayOfweek = ParsePart(parts[1], 0, 6);
-            _hour = ParsePart(timeParts[0], 0, 23);
-            _minute = ParsePart(timeParts[1], 0, 59);
-            _second = ParsePart(secondParts[0], 0, 59);
-
-            if (secondParts.Length > 1)
+            switch(parts.Length)
             {
-                _millisecond = ParsePart(secondParts[1], 0, 999);
-            }
-            else
-            {
-                _millisecond = ParsePart("0", 0, 999);
+                case 1: // Пришло только время формата 10:00:00.000
+                    ParseDate("*.*.*");
+                    _dayOfweek = ParsePart("*", 0, 6);
+                    ParseTime(parts[0]);
+                    break;
+                case 2: // Пришли дата и время без дня недели формата 26.9.26 10:00:00.000
+                    ParseDate(parts[0]);
+                    _dayOfweek = ParsePart("*", 0, 6);
+                    ParseTime(parts[1]);
+                    break;
+                case 3: // Пришла полная строка формата 26.9.26 4 10:00:00.000
+                    ParseDate(parts[0]);
+                    _dayOfweek = ParsePart(parts[1], 0, 6);
+                    ParseTime(parts[2]);
+                    break;
+                default:
+                    throw new ArgumentException("Неверный формат строки");
             }
         }
         private List<ScheduleRange> ParsePart(string text, int min, int max)
@@ -101,6 +102,15 @@ namespace Test
             }
                 return ranges;
         }
+        private void ParseDate(string date)
+        {
+            string[] dateParts = date.Split('.');
+
+            _years = ParsePart(dateParts[0], 2000, 2100);
+            _months = ParsePart(dateParts[1], 1, 12);
+            _days = ParsePart(dateParts[2], 1, 32);
+        }
+
         private void ParseTime(string time)
         {
             string[] timeParts = time.Split(':');
